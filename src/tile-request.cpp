40f576bc01a273fc3566d19bf275af839d969459
Mohammed Sameer <msameer@foolab.org>
2015-09-08 13:24:50 +0300

    Really fix the crash when we are destroying TileRequest

--- src/tile-request.cpp
+++ src/tile-request.cpp
@@ -25,25 +25,35 @@ void TileRequest::addTile(Tile& tile) {
 }
 
 void TileRequest::expire() {
-  QMutexLocker l(&m_lock);
+  m_lock.lock();
+
   m_expired = true;
 
   if (m_done) {
+    m_lock.unlock();
+
     // I have no idea why on earth would this object gets deleted before the function returns
     // causing qt to complain about destroying a locked mutex.
     QMetaObject::invokeMethod(this, "deleteLater", Qt::QueuedConnection);
   }
+
+  m_lock.unlock();
 }
 
 void TileRequest::done() {
-  QMutexLocker l(&m_lock);
+  m_lock.lock();
+
   m_done = true;
 
   if (m_expired) {
+    m_lock.unlock();
+
     // I have no idea why on earth would this object gets deleted before the function returns
     // causing qt to complain about destroying a locked mutex.
     QMetaObject::invokeMethod(this, "deleteLater", Qt::QueuedConnection);
   }
+
+  m_lock.unlock();
 }
 
 Tile TileRequest::takePending() {
